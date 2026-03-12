79ed200b8d5cbbc66677231775504384ceeda4f0
Philip Hamelink <philip.hamelink@gmail.com>
2026-03-12 14:19:27 +0100

    feat: add conversion from minml to html in the wasm implementation

--- go/wasm/main.go
+++ go/wasm/main.go
@@ -3,25 +3,31 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+
+	"github.com/dedis/matchertext/go/markup/minml"
 )
 
 // convert function that will be exposed to JS
 // args[0]: MinML input string
 // returns: HTML output string or Error string
-func convert(_ js.Value, args []js.Value) any {
+func convertString(_ js.Value, args []js.Value) any {
 	if len(args) < 1 {
 		return "Error: no input provided"
 	}
 	input := args[0].String()
 
-	fmt.Println("This is not working currently")
+	output, err := minml.ConvertString(input)
 
-	return input
+	if err != nil {
+		return fmt.Errorf("error converting input: %v", err)
+	}
+
+	return output
 }
 
 func main() {
 	// Register the function in the global JS scope
-	js.Global().Set("minmlConvert", js.FuncOf(convert))
+	js.Global().Set("minmlConvert", js.FuncOf(convertString))
 
 	fmt.Println("MinML WASM Converter Initialized")
 
